fe94dba4d0006cc04d887b1301ee2a16e4ffd90c
DubJeremy <jeremy.ah33@gmail.com>
2022-11-29 16:45:09 +0100

    (Client) Setup vue.js

---
+++ client/src/App.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import Header from "./components/Header.vue";
+import BookIcon from "./components/icons/IconBook.vue";
+</script>
+
+<template>
+    <header>
+        <div class="header">
+            <Header />
+
+            <nav>
+                <RouterLink to="/"><BookIcon /></RouterLink>
+                <RouterLink to="/profile">℗</RouterLink>
+            </nav>
+        </div>
+    </header>
+
+    <RouterView />
+</template>
+
+<style>
+header {
+    line-height: 1.5;
+    height: 30vh;
+    width: 100vw;
+    position: relative;
+    position: relative;
+}
+
+nav {
+    width: auto;
+    position: absolute;
+    right: 3%;
+}
+
+nav a.router-link-exact-active {
+    color: var(--crimson);
+}
+
+nav a.router-link-exact-active:hover {
+    background-color: transparent;
+}
+
+nav a {
+    font-size: 1.8em;
+    padding: 0 1rem;
+    border-left: 1px solid var(--white);
+}
+
+nav a:first-child {
+    padding-bottom: 5px;
+}
+
+nav a:first-of-type {
+    border: 0;
+}
+</style>
---
+++ client/src/views/ProfileView.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="profile">
+        <h1>MyCollection</h1>
+    </div>
+</template>
+
+<style></style>
